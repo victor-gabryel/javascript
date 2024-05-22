a9212f6c0fb6a5505527df3ffc472cabe472790d
Victor <victorgabryel2123@gmail.com>
2024-05-22 09:20:42 -0300

    atualizado
    aula11: ex008, ex009, ex010

---
+++ aula011/ex008.js
@@ -0,0 +1,9 @@
+var vel = 78.2
+
+console.log(`A velocidade do seu carro é ${vel} Km/h`)
+
+if (vel > 60){ // Condição Simples
+    console.log(`Voçê ultrapassou a velocidade permitida. MULTADO!`)
+} 
+
+console.log(`Dirija sempre usando o cinto de segurança`)
---
+++ aula011/ex009.js
@@ -0,0 +1,9 @@
+var país = 'Brasil'
+
+console.log(`Vivendo no país ${país}`)
+
+if (país != 'Brasil') {
+    console.log(`Estrangeiro`)
+} else {
+    console.log(`Brasileiro`)
+}
