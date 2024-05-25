42dd11dcdfa40bb8495c45dc83acedfe45cc556d
Victor <victorgabryel2123@gmail.com>
2024-05-25 19:32:41 -0300

    atualizado
    aula013, aula014

---
+++ aula013/ambiente.js
@@ -0,0 +1,23 @@
+var c = 1
+var nome = 'Victor'
+do {
+    console.log(`${nome} Ficou em ${c}°lugar`)
+    c = c + 1
+} while(c <= 100)
+
+/*
+var c = 1
+while (c <= 100){
+    console.log(`Passo ${c}`)
+    c = c + 1
+}
+*/
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
